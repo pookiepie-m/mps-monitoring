b8a428d3cc019284650af0cf2168ce477edeb1af
Firda75 <firdahesti75@gmail.com>
2025-07-28 08:56:10 +0700

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -173,7 +173,7 @@
 
     <!-- Info Boxes -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-      <div id="weather" class="bg-sky-800 border-2 border-sky-800 text-center font-semibold px-4 py-6 rounded shadow text-2xl font-bold text-sky-800">
+      <div id="weather" class="bg-sky-800 text-center font-semibold px-4 py-6 rounded shadow font-bold text-sky-800">
         <i class="fa-solid fa-bolt">24 Volt DC</i>
       </div>
       <div id="weather1" class="bg-sky-700 border-2 border-sky-800 text-center font-semibold px-4 py-6 rounded shadow text-3xl font-bold text-sky-800">
