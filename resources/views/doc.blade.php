1860978028b49086e8f03a1090cfaf728ff819ae
Firda75 <firdahesti75@gmail.com>
2025-07-28 08:21:27 +0700

    update doc halaman

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -18,5 +18,35 @@
             <p class="mt-2 text-gray-700">Keterangan Gambar 3</p>
         </div>
     </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div class="text-center">
+            <img src="{{ asset('img/gambar1.jpg') }}" alt="Gambar 1" class="w-full h-48 object-cover rounded-lg shadow">
+            <p class="mt-2 text-gray-700">Keterangan Gambar 1</p>
+        </div>
+        <div class="text-center">
+            <img src="{{ asset('img/gambar2.jpg') }}" alt="Gambar 2" class="w-full h-48 object-cover rounded-lg shadow">
+            <p class="mt-2 text-gray-700">Keterangan Gambar 2</p>
+        </div>
+        <div class="text-center">
+            <img src="{{ asset('img/gambar3.jpg') }}" alt="Gambar 3" class="w-full h-48 object-cover rounded-lg shadow">
+            <p class="mt-2 text-gray-700">Keterangan Gambar 3</p>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div class="text-center">
+            <img src="{{ asset('img/gambar1.jpg') }}" alt="Gambar 1" class="w-full h-48 object-cover rounded-lg shadow">
+            <p class="mt-2 text-gray-700">Keterangan Gambar 1</p>
+        </div>
+        <div class="text-center">
+            <img src="{{ asset('img/gambar2.jpg') }}" alt="Gambar 2" class="w-full h-48 object-cover rounded-lg shadow">
+            <p class="mt-2 text-gray-700">Keterangan Gambar 2</p>
+        </div>
+        <div class="text-center">
+            <img src="{{ asset('img/gambar3.jpg') }}" alt="Gambar 3" class="w-full h-48 object-cover rounded-lg shadow">
+            <p class="mt-2 text-gray-700">Keterangan Gambar 3</p>
+        </div>
+    </div>
 </div>
 @endsection
