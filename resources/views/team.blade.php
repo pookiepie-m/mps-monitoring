d92ea226282d53e66fa9879f8c8a0c1b28758aa8
Firda75 <firdahesti75@gmail.com>
2025-08-03 07:43:06 +0700

    ubah format gambar

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -39,21 +39,21 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-10 max-w-7xl mx-auto">
     {{-- Member khusus dengan gambar firda.PNG --}}
     <div class="bg-white rounded-lg shadow-md overflow-hidden">
-        <img src="{{ asset('/public/img/azra.JPG') }}" alt="azra" class="w-full h-60 object-cover">
+        <img src="{{ asset('/public/img/azra.jpg') }}" alt="azra" class="w-full h-60 object-cover">
         <div class="p-4 text-center">
             <h3 class="text-lg font-semibold text-gray-800">Azra Tiarani</h3>
             <p class="text-sm text-gray-600">3.32.22.2.03</p>
         </div>
     </div>
     <div class="bg-white rounded-lg shadow-md overflow-hidden">
-        <img src="{{ asset('img/gambar2.PNG') }}" alt="Firda" class="w-full h-60 object-cover">
+        <img src="{{ asset('/public/img/fir.jpg') }}" alt="Firda" class="w-full h-60 object-cover">
         <div class="p-4 text-center">
             <h3 class="text-lg font-semibold text-gray-800">Firda Pramesthi</h3>
             <p class="text-sm text-gray-600">3.32.22.2.06</p>
         </div>
     </div>
     <div class="bg-white rounded-lg shadow-md overflow-hidden">
-        <img src="{{ asset('/public/img/irpin.JPG') }}" alt="irpan" class="w-full h-60 object-cover">
+        <img src="{{ asset('/public/img/irpin.jpg') }}" alt="irpan" class="w-full h-60 object-cover">
         <div class="p-4 text-center">
             <h3 class="text-lg font-semibold text-gray-800">Irfan Hari Saputra</h3>
             <p class="text-sm text-gray-600">4.32.22.2.10</p>
