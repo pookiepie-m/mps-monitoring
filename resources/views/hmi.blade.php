e770575d08944f1ec14486b2b45abcda00d1aadd
Firda75 <firdahesti75@gmail.com>
2025-08-03 15:57:11 +0700

    ip lagi

--- resources/views/hmi.blade.php
+++ resources/views/hmi.blade.php
@@ -12,6 +12,7 @@
     <h1>Monitoring HMI</h1>
     <p>IP Address yang dicek: <span id="ip-address">10.122.222.95</span></p>
     <p>Status koneksi: <span id="status">Mengecek...</span></p>
+    
 
     <script>
         const ip = "192.168.137.1"; // Ganti dengan IP device lain
@@ -28,5 +29,5 @@
                 document.getElementById("status").style.color = "red";
             });
     </script>
-    <iframe src="10.122.222.95" width="100%" height="600px" frameborder="0"></iframe>
+    <iframe src="http://10.122.222.95/project/apps/index#page=1&now=1754210934596" width="100%" height="600px" frameborder="0"></iframe>
 @endsection
