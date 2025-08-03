a2007e26c993d6386d81cfc34fa92f9296986e61
Firda75 <firdahesti75@gmail.com>
2025-08-03 07:40:44 +0700

    ubah gambar

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -11,7 +11,7 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-10 max-w-7xl">
         @foreach ([1, 2] as $i)
         <div class="bg-white rounded-lg shadow-md overflow-hidden">
-            <img src="{{ asset('img/team' . $i . '.jpg') }}" alt="Team Member {{ $i }}" class="w-full h-60 object-cover">
+            <img src="{{ asset('/public/img/team' . $i . '.jpg') }}" alt="Team Member {{ $i }}" class="w-full h-60 object-cover">
             <div class="p-4 text-center">
                 <h3 class="text-lg font-semibold text-gray-800">Member {{ $i }}</h3>
                 <p class="text-sm text-gray-600">Unit 1 - Operator</p>
@@ -39,7 +39,7 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-10 max-w-7xl mx-auto">
     {{-- Member khusus dengan gambar firda.PNG --}}
     <div class="bg-white rounded-lg shadow-md overflow-hidden">
-        <img src="{{ asset('img/.PNG') }}" alt="azra" class="w-full h-60 object-cover">
+        <img src="{{ asset('/public/img/azra.JPG') }}" alt="azra" class="w-full h-60 object-cover">
         <div class="p-4 text-center">
             <h3 class="text-lg font-semibold text-gray-800">Azra Tiarani</h3>
             <p class="text-sm text-gray-600">3.32.22.2.03</p>
@@ -53,7 +53,7 @@
         </div>
     </div>
     <div class="bg-white rounded-lg shadow-md overflow-hidden">
-        <img src="{{ asset('img/.PNG') }}" alt="irpan" class="w-full h-60 object-cover">
+        <img src="{{ asset('/public/img/irpin.JPG') }}" alt="irpan" class="w-full h-60 object-cover">
         <div class="p-4 text-center">
             <h3 class="text-lg font-semibold text-gray-800">Irfan Hari Saputra</h3>
             <p class="text-sm text-gray-600">4.32.22.2.10</p>
